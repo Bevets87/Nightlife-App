02d7f75087363c19d7b979a49106f3fc805c06f1
Steve <bevets87@gmail.com>
2017-04-24 18:20:21 -0400

    added config variables

--- server/controllers/barControllers.js
+++ server/controllers/barControllers.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 import Bars from '../models/Bars'
 import _ from 'lodash'
-import { JWTSECRET } from '../config'
+
 
 
 export const handle_get_bars = (req, res) => {
@@ -15,7 +15,7 @@ export const handle_create_bar = (req, res) => {
 
   const { bar_id, attendee, token } = req.body
 
-  jwt.verify(token, JWTSECRET, (err, decoded) => {
+  jwt.verify(token, process.env.JWTSECRET, (err, decoded) => {
     console.log(err)
     if (!err) {
       Bars.findOne({bar_id: bar_id}, (err, bar) => {
