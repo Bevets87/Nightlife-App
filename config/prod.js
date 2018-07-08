1effe64ce513645715812ab4ffac6be9f852869e
Steve <bevets87@gmail.com>
2018-07-08 16:00:53 -0400

    fixed start script

--- config/prod.js
+++ config/prod.js
@@ -1,14 +1,7 @@
 export const port = process.env.PORT
 export const host = process.env.HOST
-export const yelp = {
-  api_key: process.env.YELP_API_KEY,
-}
-export const jwt = {
-  secret: process.env.JWT_SECRET,
-  expiration: process.env.JWT_EXPIRATION
-}
-export const db = {
-  url: process.env.MONGODB_URI
-}
+export const yelp = { api_key: process.env.YELP_API_KEY }
+export const jwt = { secret: process.env.JWT_SECRET, expiration: process.env.JWT_EXPIRATION }
+export const db = { url: process.env.MONGODB_URI }
 
 
